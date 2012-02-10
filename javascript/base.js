6698286b153faa93cd846d915c869d0507687b29
Matthew Page <matthewnpage@gmail.com>
2012-02-09 18:00:59 -0800

    add support to flickrbomb for links starting with hashes so that they aren't fetchable

--- javascript/base.js
+++ javascript/base.js
@@ -3,7 +3,8 @@
  * any dependencies you introduce via `mulberry create component`.
  */
 dojo.require('client.routes');
-dojo.require('client.flickrbomb-min');
+/* dojo.require('client.flickrbomb-min'); */
+dojo.require('client.flickrbomb');
 dojo.require('client.underscore');
 dojo.require('client.backbone');
 dojo.require('client.components.FlickrBomb');
